7fcf50a9aabc3e7ac6b87d8f5f0bde3846f34039
Roshan Kumawat <roshankumawat529@gmail.com>
2026-03-09 17:46:53 +0530

    movie app file...

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
 from src import models, movie
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "http://localhost:5173",  # Your React app's URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 app.include_router(movie.movie_route)
